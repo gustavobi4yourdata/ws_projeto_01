54e6e3ca4ef8053ef357a04a48cacfef0cb1000b
Gustavo Souza <gustavosouza@Gustavos-MacBook-Air.local>
2023-12-22 17:37:52 -0300

    transform funcionando

---
+++ app/pipeline/transform.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from typing import List
+
+"""
+função para transformar uma lista de dataframes em um único dataframe
+"""
+
+def contact_data_frame(data_frame_list: List[pd.DataFrame]) -> pd.DataFrame:
+    return pd.concat(data_frame_list, ignore_index=True)
+
+
+
---
+++ tests/test_pipeline.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from app.pipeline.transform import contact_data_frame
+
+df_1 = pd.DataFrame({'col1': [1,2], 'col2': [3,4]})
+df_2 = pd.DataFrame({'col1': [5,6], 'col2': [7,8]})
+
+
+def testar_a_concatenacao_da_lista_de_dataframe():
+    """ use o arrange act e assert para testar a função contact_data_frame"""
+
+    # arrange
+    data_frame_list = [df_1, df_2]
+    data_frame = pd.concat(data_frame_list, ignore_index=True)
+
+    # act
+    df = contact_data_frame(data_frame_list)
+
+    # assert
+    assert df.shape == (4,2)     # O corpo do dataframe é igual (4,2)
+    assert data_frame.equals(df) # Um data fica igual o outro
